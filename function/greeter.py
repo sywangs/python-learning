c23f13d55a29c7fa740022405d89f284637bbf9c
sywangs <sywang123@yeah.net>
2017-12-09 20:38:39 +0800

    function with  while
    Signed-off-by: sywangs <sywang123@yeah.net>

--- function/greeter.py
+++ function/greeter.py
@@ -21,6 +21,21 @@ def build_person (first_name,last_name,middle_name = ''):
         person['middle_name'] = middle_name
     return person
 
-person = build_person("W","SY","LL")
-print (person)
 
+# person = build_person("W","SY","LL")
+# print (person)
+
+while True:
+    print ("\n please tell me your name")
+    print ("Enter 'q' to quit at any time")
+
+    f_name = raw_input("First Name:")
+    if f_name == 'q':
+        break
+
+    l_name = raw_input("Last Name:")
+    if l_name == 'q':
+        break
+
+    full_name = get_formatted_name(f_name, l_name)
+    print ("\nHello ," + full_name)
